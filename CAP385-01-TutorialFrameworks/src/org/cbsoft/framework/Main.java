e08988ff3dfc339c753bf7d0b7828e62c106bcd2
Luiz Wagner <LuizWagner@Aline-SSHD>
2016-06-28 10:38:47 -0300

    Refatorações da Video-aula "Refatorando frameworks - Parte 1 - Herança, composição e composição recursiva"

--- CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Main.java
+++ CAP385-01-TutorialFrameworks/src/org/cbsoft/framework/Main.java
@@ -5,12 +5,20 @@ public class Main {
 	public static void main(String[] args){
 		Product p = new Product("notebook", "HP", 1999.99, "2348203894032948");
 		
-		CompressedXMLSerializer cxs = new CompressedXMLSerializer();
+		FileSerializer cxs = new FileSerializer(new Compressor(), new XMLFormatter());
 		cxs.generateFile("product.zip", p);
 		
-		CryptoPropertiesSerializer sps = new CryptoPropertiesSerializer(5);
+		FileSerializer sps = new FileSerializer(new Crypto(5), new PropertiesFormatter());
 		sps.generateFile("product.txt", p);
 		
+		FileSerializer zipProp = new FileSerializer(new Compressor(), new PropertiesFormatter());
+		zipProp.generateFile("properties.zip", p);
+		
+		CompositePostProcessor cpp = new CompositePostProcessor(new Crypto(5), new Compressor());
+		FileSerializer compositeCryptoZipped = new FileSerializer(cpp, new PropertiesFormatter());
+		compositeCryptoZipped.generateFile("compositeCryptoZipped.zip", p);
+		
+		System.out.println("Done!");
 	}
 
 }
